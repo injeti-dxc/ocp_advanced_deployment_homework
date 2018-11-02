1f87dfa6f1cc4b6ca8ccc6926545993196db448b
injeti-dxc <44522151+injeti-dxc@users.noreply.github.com>
2018-11-02 15:29:48 +0530

    Update add-users.sh

--- scripts/add-users.sh
+++ scripts/add-users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-for user in Manohar ramu latha raja; do htpasswd -b /etc/origin/master/htpasswd $user r3dh4t1!;
+touch /etc/origin/master/htpasswd
+for user in manohar ramu latha raja; do htpasswd -b /etc/origin/master/htpasswd $user r3dh4t1!;
 done
