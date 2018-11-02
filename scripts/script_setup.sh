d75014baf9873266da92086f8b91d2a562e4933b
injeti-dxc <44522151+injeti-dxc@users.noreply.github.com>
2018-11-02 12:11:22 +0530

    Update script_setup.sh

--- scripts/script_setup.sh
+++ scripts/script_setup.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
+chmod +x ./scripts/add-users.sh
+chmod +x ./scripts/create_pvs.sh
+chmod +x ./scripts/generate_inventory.sh
+chmod +x ./scripts/multitenancy.sh
+chmod +x ./scripts/pvs_10Gsize.sh
+chmod +x ./scripts/pvs_5Gsize.sh
+chmod +x ./scripts/uninstall.sh
 
 sed -i -e 's/\r$//' ./scripts/add-users.sh
 sed -i -e 's/\r$//' ./scripts/create_pvs.sh
